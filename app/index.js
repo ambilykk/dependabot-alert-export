ee4763c29c035403cf2829936bd111d9d1c4d5a3
Daniel Figucio <745341+affrae@users.noreply.github.com>
2022-09-20 17:52:20 +1000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -55,56 +55,64 @@ const query =
       }
     }`
 
-// Our CSV output fields
-const fields = [{
-  label: 'Id',
-  value: 'id'
-},
-{
-  label: 'State',
-  value: 'state'
-},
-{
-  label: 'Scope',
-  value: 'dependencyScope'
-},
-{
-  label: 'Created At',
-  value: 'createdAt'
-},
-{
-  label: 'Manifest File Name',
-  value: 'vulnerableManifestFilename'
-},
-{
-  label: 'Vulnerability Version Range',
-  value: 'securityVulnerability.vulnerableVersionRange'
-},
-{
-  label: 'Package Name',
-  value: 'securityVulnerability.package.name'
-},
-{
-  label: 'GHSA Id',
-  value: 'securityAdvisory.ghsaId'
-},
-{
-  label: 'Severity',
-  value: 'securityAdvisory.severity'
-},
-{
-  label: 'Summary',
-  value: 'securityAdvisory.summary'
-},
-{
-  label: 'Link',
-  value: 'securityAdvisory.permalink'
-},
-{
-  label: 'Description',
-  value: 'securityAdvisory.description'
-}
-];
+    // ALERT! - grab the repo name and owner login from the result data
+      // Our CSV output fields
+  const fields = [{
+      label: 'Repository',
+      value: 'repository.name'
+    },
+    {
+      label: 'Organization',
+      value: 'repository.owner.login'
+    },
+    {
+      label: 'Id',
+      value: 'repository.vulnerabilityAlerts.nodes.id'
+    },
+    {
+      label: 'State',
+      value: 'repository.vulnerabilityAlerts.nodes.state'
+    },
+    {
+      label: 'Scope',
+      value: 'repository.vulnerabilityAlerts.nodes.dependencyScope'
+    },
+    {
+      label: 'Created At',
+      value: 'repository.vulnerabilityAlerts.nodes.createdAt'
+    },
+    {
+      label: 'Manifest File Name',
+      value: 'repository.vulnerabilityAlerts.nodes.vulnerableManifestFilename'
+    },
+    {
+      label: 'Vulnerability Version Range',
+      value: 'repository.vulnerabilityAlerts.nodes.securityVulnerability.vulnerableVersionRange'
+    },
+    {
+      label: 'Package Name',
+      value: 'repository.vulnerabilityAlerts.nodes.securityVulnerability.package.name'
+    },
+    {
+      label: 'GHSA Id',
+      value: 'repository.vulnerabilityAlerts.nodes.securityAdvisory.ghsaId'
+    },
+    {
+      label: 'Severity',
+      value: 'repository.vulnerabilityAlerts.nodes.securityAdvisory.severity'
+    },
+    {
+      label: 'Summary',
+      value: 'repository.vulnerabilityAlerts.nodes.securityAdvisory.summary'
+    },
+    {
+      label: 'Link',
+      value: 'repository.vulnerabilityAlerts.nodes.securityAdvisory.permalink'
+    },
+    {
+      label: 'Description',
+      value: 'repository.vulnerabilityAlerts.nodes.securityAdvisory.description'
+    }];
 
 // graphql query execution
 async function getAlerts(org, repo, pagination) {
@@ -129,27 +137,12 @@ async function run(org_Name, repo_Name, csv_path) {
     do {
       // invoke the graphql query execution
       await getAlerts(org_Name, repo_Name, pagination).then(alertResult => {
-        let vulnerabilityNodes = alertResult.repository.vulnerabilityAlerts.nodes;
-
-        // ALERT! - grab the repo name and owner login from the result data
-        let extraFields = [{
-          label: 'Repository',
-          value: 'alertResult.repository.name'
-        },
-        {
-          label: 'Organization',
-          value: 'alertResult.repository.owner.login'
-        }
-        ];
-
-        // ALERT! - Mash all the fields together
-        const allFields = extraFields.concat(fields);
 
         // ALERT! - create our updated opts
         const opts = { allFields, "header": addTitleRow };
   
         // append to the existing file (or create and append if needed)
-        require("fs").appendFileSync(csv_path, `${parse(vulnerabilityNodes, opts)}\n`);
+        require("fs").appendFileSync(csv_path, `${parse(alertResult, opts)}\n`);
 
         // pagination to get next page data
         let pageInfo = alertResult.repository.vulnerabilityAlerts.pageInfo;
